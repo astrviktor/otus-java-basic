e7d1e08c21d52ea050d93d328d7fac057ca8facc
astrviktor <astrviktor@gmail.com>
2024-06-04 14:22:44 +0300

    Deleted comments

--- hw09/ru/otus/java/basic/MainApp.java
+++ hw09/ru/otus/java/basic/MainApp.java
@@ -9,27 +9,18 @@ import java.util.List;
 
 public class MainApp {
     public static void main(String[] args) {
-        // Реализуйте метод, принимающий в качестве аргументов числа min и max, и возвращающий ArrayList
-        // с набором последовательных значений в указанном диапазоне (min и max включительно, шаг - 1)
         ArrayList<Integer> list = createArrayListBetweenMinMax(1,10);
         System.out.println(list);
 
-        // Реализуйте метод, принимающий в качестве аргумента список целых чисел, суммирующий все элементы,
-        // значение которых больше 5, и возвращающий сумму;
         int sum = sumArrayListElemMoreThan5(list);
         System.out.println("Сумма элементов которые больше 5: " + sum);
 
-        // Реализуйте метод, принимающий в качестве аргументов целое число и ссылку на список, метод должен
-        // переписать каждую заполненную ячейку списка указанным числом;
         setNumberToArrayListElems(list, 3);
         System.out.println(list);
 
-        // Реализуйте метод, принимающий в качестве аргументов целое число и ссылку на список,
-        // увеличивающий каждый элемент списка на указанное число;
         addNumberToArrayListElems(list, 1);
         System.out.println(list);
 
-        // Создайте класс Сотрудник с полями: имя, возраст;
         List<Employee> employeeList = new ArrayList<>(Arrays.asList(
                 new Employee("Сотрудник 1", 25),
                 new Employee("Сотрудник 2", 20),
@@ -38,24 +29,17 @@ public class MainApp {
                 new Employee("Сотрудник 5", 45)
         ));
 
-        // Реализуйте метод, принимающий в качестве аргумента список сотрудников, и возвращающий список их имен;
         List<String> namesList = getEmployeeNameList(employeeList);
         System.out.println("Имена сотрудников: " + namesList);
 
-        // Реализуйте метод, принимающий в качестве аргумента список сотрудников и минимальный возраст, и возвращающий
-        // список сотрудников, возраст которых больше либо равен указанному аргументу;
         int minAge = 25;
         List<Employee> checkMinAgeList = checkEmployeeMinAge(employeeList, minAge);
         System.out.println("Список сотрудников с возрастом >= " + minAge + ": " + checkMinAgeList);
 
-        // Реализуйте метод, принимающий в качестве аргумента список сотрудников и минимальный средний возраст,
-        // и проверяющий, что средний возраст сотрудников превышает указанный аргумент;
         int averageAge = 20;
         boolean check = checkEmployeeAverageAge(employeeList, averageAge);
         System.out.println("Средний возраст > " + averageAge + ": "+ check);
 
-        // Реализуйте метод, принимающий в качестве аргумента список сотрудников,
-        // и возвращающий ссылку на самого молодого сотрудника.
         Employee youngest = findYoungestEmployee(employeeList);
         System.out.println("Самый молодой: " + youngest);
     }
